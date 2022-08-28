6f70366428885fe9a7033d57608b4ca17536976e
Lastnight77 <lastnight0@proton.me>
2022-08-28 20:46:55 +0200

    Work on collision system

--- main.js
+++ main.js
@@ -63,60 +63,210 @@ class Entity{
         let mx
         let my
         
-        let move = true
+        let movex = true
+        let movey = true
 
 
         
         //normalize and generate step
-        if ( (dx !== 0 || dy !== 0) && this.speed >= 0){
+        if ( dx !== 0 && this.speed > 0){
             mx = ( dx  / Math.sqrt(dx ** 2 + dy ** 2) ) * this.speed
+        } 
+        else{
+            movex = false
+        }
+
+        if ( dy !== 0 && this.speed > 0){
             my = ( dy  / Math.sqrt(dx ** 2 + dy ** 2) ) * this.speed
         } 
         else{
-            move = false
+            movey = false
         }
 
-        let originalSpeed = this.speed
-        this.speed = this.speed - 0.5
-        
-        //check if destination is inside solid entities
+
+        let blocked = false
+
         for (let i = 0 ; i < entities.length ; i++){
 
-            if( entities[i].collision == true ){
+            if( entities[i].collision == true && blocked == false){
+
+                let theOneThatBlocked
+
+                //praticamente prendo la funzione degli edge del player e dell'entity e vedo se il punto in comunque appartiene sia alla distanza coperta dal player che al lato dell'entity
+
+
+                //MOVEX
+                //se il punto [leftEdge||rightEdge;player.y] è compreso SIA tra (player.x,player.x+mx) e (En.y+En.y+height)
                 
+                let maxX = 0
+                let minX = 0
+
+                if(mx > 0){
+                    maxX = this.x + mx
+                    minX = this.x
+                }else{
+                    maxX = this.x
+                    minX = this.x + mx
+                }
+
+
+                //UL per mx per leftEdge e poi rightEdge
+                if( ( entities[i].x >= minX && entities[i].x <= maxX) && ( this.y >= entities[i].y && this.y <= entities[i].y + entities[i].width) ){
+                    movex = false;
+                    theOneThatBlocked = i
+                    blocked = true
+                }
+                /*if( (entities[i].x + entities[i].width > this.x && entities[i].x + entities[i].width < this.x + mx ) && ( this.y > entities[i].y && this.y < entities[i].y + entities[i].width) ){
+                    movex = false;
+                    theOneThatBlocked = i
+                    blocked = true
+                }*/
+
+                /*
+                //UR per mx per leftEdge e poi rightEdge
+                if( ( entities[i].x > this.x + this.width && entities[i].x < this.x + this.width + mx) && ( this.y > entities[i].y && this.y < entities[i].y + entities[i].width) ){
+                    movex = false;
+                    theOneThatBlocked = i
+                    blocked = true
+                }
+                if( (entities[i].x + entities[i].width > this.x + this.width && entities[i].x + entities[i].width < this.x + this.width + mx ) && ( this.y > entities[i].y && this.y < entities[i].y + entities[i].width) ){
+                    movex = false;
+                    theOneThatBlocked = i
+                    blocked = true
+                }
+
+
+                //LowerL per mx per leftEdge e poi rightEdge
+                if( ( entities[i].x > this.x && entities[i].x < this.x + mx) && ( this.y + this.height > entities[i].y && this.y + this.height < entities[i].y + entities[i].width) ){
+                    movex = false;
+                    theOneThatBlocked = i
+                    blocked = true
+                }
+                if( (entities[i].x + entities[i].width > this.x && entities[i].x + entities[i].width < this.x + mx ) && ( this.y + this.height > entities[i].y && this.y + this.height < entities[i].y + entities[i].width) ){
+                    movex = false;
+                    theOneThatBlocked = i
+                    blocked = true
+                }
+
+
+                //LowerR per mx per leftEdge e poi rightEdge
+                if( ( entities[i].x > this.x + this.width && entities[i].x < this.x + this.width + mx) && ( this.y + this.height > entities[i].y && this.y + this.height < entities[i].y + entities[i].width) ){
+                    movex = false;
+                    theOneThatBlocked = i
+                    blocked = true
+                }
+                if( (entities[i].x + entities[i].width > this.x + this.width && entities[i].x + entities[i].width < this.x + this.width + mx ) && ( this.y + this.height > entities[i].y && this.y + this.height < entities[i].y + entities[i].width) ){
+                    movex = false;
+                    theOneThatBlocked = i
+                    blocked = true
+                }
+                */
+               console.log(this.x,this.y)
+                
+                
+                
+                
+                
+                
+                
+                /*
+                //Check corners for X
                 //check upper left corner
-                if ( (this.x + mx > entities[i].x && this.x + mx < (entities[i].x + entities[i].width ) ) && (this.y + my > entities[i].y && this.y + my < (entities[i].y + entities[i].height)))
-                    move = false;
+                if ( (this.x + mx > entities[i].x && this.x + mx < (entities[i].x + entities[i].width ) ) && (this.y > entities[i].y && this.y < (entities[i].y + entities[i].height)))
+                    {
+                    movex = false;
+                    theOneThatBlocked = i
+                    blocked = true
+                }
                 
                 //check upper right corner 
-                if ( (this.x + mx + this.width > entities[i].x && this.x + mx + this.width < (entities[i].x + entities[i].width ) ) && (this.y + my > entities[i].y && this.y + my < (entities[i].y + entities[i].height)))
-                    move = false;
+                if ( (this.x + mx + this.width > entities[i].x && this.x + mx + this.width < (entities[i].x + entities[i].width ) ) && (this.y > entities[i].y && this.y < (entities[i].y + entities[i].height)))
+                    {movex = false;
+                    theOneThatBlocked = i
+                    blocked = true}
 
                 //check bottom left corner
-                if ( (this.x + mx > entities[i].x && this.x + mx < (entities[i].x + entities[i].width ) ) && (this.y + my + this.height > entities[i].y && this.y + my + this.height < (entities[i].y + entities[i].height)))
-                    move = false;
+                if ( (this.x + mx > entities[i].x && this.x + mx < (entities[i].x + entities[i].width ) ) && (this.y  + this.height > entities[i].y && this.y + this.height < (entities[i].y + entities[i].height)))
+                    {movex = false;
+                    theOneThatBlocked = i
+                    blocked = true}
 
                 //check bottom right corner
-                if ( (this.x + mx + this.width > entities[i].x && this.x + mx + this.width < (entities[i].x + entities[i].width ) ) && (this.y + my + this.height > entities[i].y && this.y + my + this.height < (entities[i].y + entities[i].height)))
-                    move = false; 
+                if ( (this.x + mx + this.width > entities[i].x && this.x + mx + this.width < (entities[i].x + entities[i].width ) ) && (this.y + this.height > entities[i].y && this.y + this.height < (entities[i].y + entities[i].height)))
+                    {movex = false;
+                    theOneThatBlocked = i
+                    blocked = true} 
+
                 
-            }
+                
+                //Check corners for Y
+                //check upper left corner
+                if ( (this.x > entities[i].x && this.x < (entities[i].x + entities[i].width ) ) && (this.y + my > entities[i].y && this.y + my < (entities[i].y + entities[i].height)))
+                    {movey = false;
+                    theOneThatBlocked = i
+                    blocked = true}
+                
+                //check upper right corner 
+                if ( (this.x + this.width > entities[i].x && this.x + this.width < (entities[i].x + entities[i].width ) ) && (this.y + my > entities[i].y && this.y + my < (entities[i].y + entities[i].height)))
+                    {movey = false;
+                    theOneThatBlocked = i
+                    blocked = true}
 
-            if(move == false && this.speed > 0.5){
+                //check bottom left corner
+                if ( (this.x > entities[i].x && this.x < (entities[i].x + entities[i].width ) ) && (this.y + my + this.height > entities[i].y && this.y + my + this.height < (entities[i].y + entities[i].height)))
+                    {movey = false;
+                    theOneThatBlocked = i
+                    blocked = true}
 
-                console.log(this.speed)
-                //this.move([dx,dy])
+                //check bottom right corner
+                if ( (this.x + this.width > entities[i].x && this.x + this.width < (entities[i].x + entities[i].width ) ) && (this.y + my + this.height > entities[i].y && this.y + my + this.height < (entities[i].y + entities[i].height)))
+                    {movey = false;
+                    theOneThatBlocked = i
+                    blocked = true}
+                */
+                /*
+                //Cover remaining gap
+                if (movex == false && dx !== 0){
+                    let gap = 0
 
+                    if(dx > 0){
+                        gap = entities[theOneThatBlocked].x - (this.x + this.width)
+                        this.x += Math.floor(gap)
+                    }
+
+                    if(dx < 0){
+
+                        gap = this.x - (entities[theOneThatBlocked].x + entities[theOneThatBlocked].width)
+                        this.x -= Math.floor(gap)
+                    }
+
+                    console.log(gap)
+                    
+
+                }
+
+                if (movey == false && dy !== 0){
+                    let gap = 0
+                    if(dy > 0)
+                        gap = entities[theOneThatBlocked].y - (this.y + this.height)
+                    if(dy < 0)
+                        gap = (this.y - (entities[theOneThatBlocked].y + entities[theOneThatBlocked].height)) * -1
+
+                    //if(gap >= 1)
+                        //this.y += gap
+                }  */
             }
         }
 
-
-        if( move == true) {
+        
+        if( movex == true) {
             this.x += mx
+        }
+        if( movey == true){
             this.y += my
         }
 
-        this.speed = originalSpeed
+        //this.speed = originalSpeed
 
 
     }
@@ -233,7 +383,9 @@ function create ()
     var upperEdge = new wall(0,0, dimensione_x , wallThickness)
     var rightEdge = new wall(dimensione_x - wallThickness , 0 , wallThickness , dimensione_y)
     var leftEdge = new wall(0,0, wallThickness , dimensione_y)
-    var centerWall = new wall(200,200,30,30)
+    var centerWall = new wall(200,200,50,30)
+    var thinWall = new wall(100,300,50,4)
+    var thinWall2 = new wall(100,400,4,50)
 
 
     fpsCounter = this.add.text(20, 15);
